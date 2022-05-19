f624278f2e5bd631c3c87c1ed67e9ffe706ceb29
Tal <talrofeh@gmail.com>
2022-05-19 13:58:44 +0300

    WIP

--- BetterEducationToolbar/CollectAssetsPatch.cs
+++ BetterEducationToolbar/CollectAssetsPatch.cs
@@ -7,14 +7,14 @@ using static GeneratedGroupPanel;
 namespace BetterEducationToolbar
 {
 	// This patch overrides the list of tabs that will be listed in the Education toolbar (not their contents!).
-	[HarmonyPatch(typeof(GeneratedGroupPanel), "CollectAssets")]
-    class CollectAssetsPatch
+	[HarmonyPatch(typeof(GeneratedGroupPanel))]
+    class GeneratedGroupPanelPatch
     {
+		[HarmonyPatch(typeof(GeneratedGroupPanel), "CollectAssets")]
 		[HarmonyPostfix]
         public static void Postfix(GroupFilter filter, Comparison<GroupInfo > comparison, ref PoolList<GroupInfo> __result, GeneratedGroupPanel __instance)
         {
-			if (!filter.IsFlagSet(GroupFilter.Building) || __instance.service != ItemClass.Service.Education ||
-				!Mod.IsInGame() )
+			if (!filter.IsFlagSet(GroupFilter.Building) || __instance.service != ItemClass.Service.Education ||	!Mod.IsInGame())
 			{
 				return;
 			}
@@ -32,17 +32,14 @@ namespace BetterEducationToolbar
 			__result.Clear();
 
 			var educationCategoriesNeeded = new List<EducationCategory>();
-			var miscCategoriesNeeded = new List<string>();
 
 			var toolManagerExists = Singleton<ToolManager>.exists;
 
 			for (uint i = 0u; i < PrefabCollection<BuildingInfo>.LoadedCount(); ++i)
 			{
 				BuildingInfo info = PrefabCollection<BuildingInfo>.GetLoaded(i);
-				if (info != null &&
-					info.GetService() == ItemClass.Service.Education || info.GetService() == ItemClass.Service.PlayerEducation &&
-					(!toolManagerExists || info.m_availableIn.IsFlagSet(Singleton<ToolManager>.instance.m_properties.m_mode)) &&
-					info.m_placementStyle == ItemClass.Placement.Manual)
+				if (info != null && info.GetService() == ItemClass.Service.Education && (!toolManagerExists || 
+					info.m_availableIn.IsFlagSet(Singleton<ToolManager>.instance.m_properties.m_mode)) && info.m_placementStyle == ItemClass.Placement.Manual)
 				{
 					if (!EducationUtils.IsEducationCategory(info.category))
                     {
@@ -74,5 +71,26 @@ namespace BetterEducationToolbar
 				__result.Add(miscTab);
             }
 		}
-    }
+    
+		
+		[HarmonyPatch(typeof(GeneratedGroupPanel), "RefreshPanel")]
+		[HarmonyPrefix]
+		public static bool RefreshPanel(GeneratedGroupPanel __instance)
+		{
+			if(__instance.service == ItemClass.Service.Education)
+            {
+				__instance.PopulateGroups(__instance.groupFilter, __instance.sortingMethod);
+				return false;
+            } 
+			else
+            {
+				return true;
+            }
+		}
+
+	}
+
+
+	
+
 }
